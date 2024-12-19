a8ca82a8e80557bdefeb158d53d1c14c27a57dad
OPO113697 <OPO113697@OMUMLAP-162>
2024-12-19 14:14:10 +0530

    Bug Fixing

--- obj/Debug/net6.0/QMS.AssemblyInfo.cs
+++ obj/Debug/net6.0/QMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e517fafccfc3af8b233976ee8a0dcc96593cc5cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2ea3b93c566d788daf7dfaa57c530ee7d038dee")]
 [assembly: System.Reflection.AssemblyProductAttribute("QMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
