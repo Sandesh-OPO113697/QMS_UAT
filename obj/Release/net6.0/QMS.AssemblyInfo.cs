e9c2ed21241fb9072d4a80551f81262914763ab7
Sandesh Bhosale <sandesh.bhosale@1point1.com>
2025-03-11 15:15:37 +0530

    Bug in sprint 3

--- obj/Release/net6.0/QMS.AssemblyInfo.cs
+++ obj/Release/net6.0/QMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de4a2f550982b736666451f15677de4dd87ab561")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b303b957ad59680768c8d995f180127ec40794ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("QMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
