866f3bc6999f3f92dc29805e8ea854e4b0ebf31f
Sandesh Bhosale <sandesh.bhosale@1point1.com>
2025-03-28 17:11:46 +0530

    Final Sprint 4

--- obj/Debug/net6.0/QMS.AssemblyInfo.cs
+++ obj/Debug/net6.0/QMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e19b4202bfc217dac2fba507da9449211f0d29c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13e8114e588279ae1579b4de133736507ec25f90")]
 [assembly: System.Reflection.AssemblyProductAttribute("QMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
