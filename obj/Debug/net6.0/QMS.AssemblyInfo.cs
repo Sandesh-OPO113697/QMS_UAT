1850b25218d3528d1dac479219caf37ff99293b4
Sandesh Bhosale <sandesh.bhosale@1point1.com>
2025-05-14 16:47:28 +0530

    Bug

--- obj/Debug/net6.0/QMS.AssemblyInfo.cs
+++ obj/Debug/net6.0/QMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ce40f43320256cb61f2418128a3b33f33ee9d27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b601bbaa3d40a00038ade150c8219512f2d54f72")]
 [assembly: System.Reflection.AssemblyProductAttribute("QMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
