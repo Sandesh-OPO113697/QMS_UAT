30178d963fbaf3b76ecf0e92d263cab186e723f5
Rahul Shembdekar <OPO113177@onepointone.in>
2025-06-09 12:29:57 +0530

    Desgin

--- obj/Debug/net6.0/QMS.AssemblyInfo.cs
+++ obj/Debug/net6.0/QMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f96ae4e274dfa39405670aa5df1a56625846d2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55ff831d57f6b0f0083d932e7172377b369c5121")]
 [assembly: System.Reflection.AssemblyProductAttribute("QMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
